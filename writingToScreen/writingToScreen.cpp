f2da5164443de68b757be073b35ce170ea5b76fa
= <lokhandwala.riz1@gmail.com>
2025-01-05 13:06:22 -0600

    built the skeleton of the program

---
+++ writingToScreen/writingToScreen.cpp
@@ -0,0 +1,19 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+
+int main(int argc, char* args[])
+{
+
+}
