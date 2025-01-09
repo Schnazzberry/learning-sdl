23ae2ba0d0a23172d89e0d18c312d7c56666f026
= <lokhandwala.riz1@gmail.com>
2025-01-09 10:50:31 -0600

    added eventDrivenProgramming/ and keyPresses.cpp

---
+++ eventDrivenProgramming/keyPresses.cpp
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+//Key press surfaces constants
+enum KeyPressSurfaces
+{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
