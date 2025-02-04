a308f77e4def1c112504d62856b2cb05907c1871
= <lokhandwala.riz1@gmail.com>
2025-02-04 09:22:54 -0600

    Created loadingPNGs set up dependencies

---
+++ loadingPNGs/loadingPNGs.cpp
@@ -0,0 +1,12 @@
+//Using SDL, SDL_image, standard IO, and strings
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+int main()
+{
+    printf("Hello World\n");
+
+    return 0;
+}
